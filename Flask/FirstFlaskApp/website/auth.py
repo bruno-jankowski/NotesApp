f916e345edf7b46ce804b88d2a1e60d1f6d6bf19
BrunoJankowski <jankowskibruno2@gmail.com>
2023-03-01 21:18:45 +0100

    2nd commit
    I added a note system, implemented my css customization, dealed with some problems on my own so solution is different (still in pregress: dealiting notes, navbar change)

--- Flask/FirstFlaskApp/website/auth.py
+++ Flask/FirstFlaskApp/website/auth.py
@@ -1,41 +1,72 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user ##module of flask used for passing login user
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password') ##This referes to the email and password we posted so what we type in
+
+        user = User.query.filter_by(email=email).first() ##returns the first resoult of filtering by column email so it finds the user with this column email and takkes all of his variables from a class
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully', category='valid')
+                login_user(user, remember=True)
+                return redirect(url_for('auth.gallery'))
+            else:
+                flash('Inccorect password, try again', category='error')
+        else:
+            flash('There is no such user', category='error')
+
     return render_template("login.html")
 # passing variable by adding it after loginhtml user="bruno", userList=["maja", "antek", "bruno"] (for instance and u can write if statment in loi)
 
 
 @auth.route('/logout')
+@login_required    ##this decorations we add to the pages user can not access unless they are login in 
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        if len(email) < 4:
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exist', category='error')
+        elif len(email) < 4:
             flash(' Enter a proper email', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash(' Name must have more then 2 characters', category='error')
         elif password1 != password2:
             flash(' Passwords do not match', category='error')
         elif len(password1) < 7:
             flash(' Password must have more than 7 characters', category='error')
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256')) ##hashing the password and passing it to the user (sha256 is method  of hashing)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
             flash('You created an account', category='valid')
-    return render_template("sign_up.html")
+            return redirect(url_for('views.home'))
+   
+    user = current_user   
+    return render_template("sign_up.html", user=user)
 
 
 @auth.route('/gallery')
+@login_required
 def gallery():
     return render_template("gallery.html")
--- Flask/FirstFlaskApp/website/views.py
+++ Flask/FirstFlaskApp/website/views.py
@@ -1,9 +1,36 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, jsonify, render_template, request, flash
+from flask_login import login_required, current_user ##module of flask used for passing login user (if user is loged in current_user refers to all of the data from user model)
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is to short', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added', category='valid')            
+    user = current_user
+    return render_template("home.html", user=user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
